4e7234e1abc321891f961a3b147cf35c38e2de6c
Vladimir Sorokin <sorokinv@hotmail.com>
2021-06-07 20:45:35 +1000

    test on logger.py

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -2,30 +2,6 @@ import logging
 import sys, os
 
 from  binance_trade_bot.logger import Logger
-#from a.logger import Logger
-
-class test_Logger:
-    def test_log(self):
-        assert False
-
-    def test_info(self):
-        assert False
-
-    def test_warning(self):
-        assert False
-
-    def test_error(self,capsys):
-        logs = Logger().error('eerroorr')
-        assert False
-
-    def test_debug(self,capsys):
-        logs.debug('gguubbeedd', notification=True)
-        captured = capsys.readouterr()
-        assert str(captured).find('DEBUG') > -1
-
-        assert False
-
-# comment backtest.py : cache=SqliteDict(...)
 
 def test_log1(capsys) :
 
@@ -74,11 +50,14 @@ def test_log2(capsys):
 
     logs2 = Logger(enable_notifications=False, logging_service='boba_boba')
 
-    #assert os.path.exists(os.path.join('logs','boba_boba.log')) , "Default log file not exists"
+    assert os.path.exists(os.path.join('logs','boba_boba.log')) , "Random log file not exists"
 
     logs2.warning('ggnniinnrraaww',notification=False)
     captured = capsys.readouterr()
     print('\n',captured)
-    assert len(str(captured))==0
+    assert len(str(captured))==0 , 'Notification==False , but informing?'
+
+    if os.path.exists(os.path.join('logs', 'boba_boba.log')) :
+        os.remove(os.path.join('logs', 'boba_boba.log'))
 
     assert True

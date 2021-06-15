5bff408e4dc596faccb0e6b182bea6e3a4bf2e15
Vladimir Sorokin <sorokinv@hotmail.com>
2021-06-15 12:30:20 +1000

    Testing test_default_strategy.py

---
+++ tests/test_default_strategy.py
@@ -0,0 +1,13 @@
+class TestStrategy:
+
+    def test_initialize(self):
+        assert False
+
+    def test_scout(self):
+        assert False
+
+    def test_bridge_scout(self):
+        assert False
+
+    def test_initialize_current_coin(self):
+        assert False
